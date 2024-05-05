97b33c9e508da802295da3e77a2c52ba7b110e65
Rebecca Voll <doktavoll@gmail.com>
2024-05-05 21:02:10 +0200

    fix form.html and mainly form.css so that entered text is left-aligned

--- form.js
+++ form.js
@@ -3,3 +3,18 @@
 // The following acceptance criteria should be met for the bookmark button:
 
 console.clear();
+
+// The data entered into the form by users should be used to create a new question
+// that will be displayed as a card like the other questions.
+
+// Listen to the form's submit event
+
+// Prevent the default submit behavior to handle everything within JavaScript
+
+// Read all entered data from the input fields (question, answer, tags)
+// Generate all DOM element for a card with createElement()
+// Insert the form's data as text into the DOM elements
+
+// Append the card to the page, directly below the form
+
+// console.log("sanity check");
